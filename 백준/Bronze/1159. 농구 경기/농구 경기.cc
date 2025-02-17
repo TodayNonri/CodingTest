5b14f9408692bec8f807a2c41ba3bbace026f92d
TodayNonri <126966916+TodayNonri@users.noreply.github.com>
2025-02-17 17:33:56 +0900

    [Bronze II] Title: 농구 경기, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Bronze/1159. 농구 경기/농구 경기.cc
+++ 백준/Bronze/1159. 농구 경기/농구 경기.cc
@@ -6,36 +6,30 @@
 using namespace std;
 
 int n;
-string temp;
+string temp, result;
 vector<string> vName;
 int main()
 {
 
 	cin >> n;
-	for (int i = 0; i < n; ++i)
-	{
-		cin >> temp;
-		vName.push_back(temp);
-	}
 	int alpha[26] = {};
 	for (int i = 0; i < n; ++i)
 	{
-		int index = vName[i].front() - 'a';
-		alpha[index]++;
+		cin >> temp;
+		alpha[temp.front() - 'a']++;
 	}
 
-	bool flag = false;
 	for (int i = 0; i < 26; ++i)
 	{
 		if (alpha[i] >= 5)
-		{
-			flag = true;
-			cout << (char)(i + 'a');
-		}
+			result+=((char)(i + 'a'));
 	}
+
+	if (result.empty())
+		cout << "PREDAJA";
+	else
+		cout << result;
 	
-	if(flag == false)
-		cout << "PREDAJA" << "\n";
 
 	return 0;
 }
