8b03a341f86e1c7eb30db5ef04cce967bfa96e2a
TodayNonri <126966916+TodayNonri@users.noreply.github.com>
2025-02-18 10:57:14 +0900

    [Silver III] Title: 수열, Time: 28 ms, Memory: 2412 KB -BaekjoonHub

--- 백준/Silver/2559. 수열/수열.cc
+++ 백준/Silver/2559. 수열/수열.cc
@@ -6,33 +6,23 @@
 using namespace std;
 
 int n, k;
-vector<int> vDay;
-vector<int> vMax;
+int temp, psum[100001], ret = -10000004;
+
 int main()
 {
 	cin >> n>> k;
 
-	for (int i = 0; i < n; ++i)
+	for (int i = 1; i <= n; ++i)
 	{
-		int iInput;
-		cin >> iInput;
-		vDay.push_back(iInput);
+		cin >> temp;
+		psum[i] = psum[i - 1] + temp;
 	}
 
-	for (int i = 0; i <= n - k; ++i)
+	for (int i = k; i <= n; ++i)
 	{
-		int iSum = 0;
-		for (int j = i; j < i + k; ++j)
-		{
-			iSum += vDay[j];
-		}
-		vMax.push_back(iSum);
+		ret = max(ret, psum[i] - psum[i - k]);
 	}
+	cout << ret << "\n";
 
-	sort(vMax.begin(), vMax.end());
-
-	cout << vMax.back() << "\n";
-
-	
 	return 0;
 }
