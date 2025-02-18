03332701a072b45358f297f3b9169d87791c2aeb
TodayNonri <126966916+TodayNonri@users.noreply.github.com>
2025-02-18 11:21:27 +0900

    [Silver IV] Title: 나는야 포켓몬 마스터 이다솜, Time: 192 ms, Memory: 25528 KB -BaekjoonHub

--- 백준/Silver/1620. 나는야 포켓몬 마스터 이다솜/나는야 포켓몬 마스터 이다솜.cc
+++ 백준/Silver/1620. 나는야 포켓몬 마스터 이다솜/나는야 포켓몬 마스터 이다솜.cc
@@ -1,37 +1,38 @@
 #include <iostream>
-#include <string>
+#include <algorithm>
 #include <vector>
+#include <string>
 #include <map>
 using namespace std;
-//해시맵사용
-int main(){
-    map<string, int> pokemon;
-    vector<string> name;
-    int n, m;
-    string temp; //포켓몬 이름과 번호가 섞여있으므로 string
-    cin >> n >> m;
-    vector<string> result; 
-    for (int i = 1; i <= n;i++){
-        cin >> temp; 
-        name.push_back(temp); // 도감번호 주어질때 포켓몬 이름 찾기
-        pokemon.insert(make_pair(temp, i)); // 포켓몬 이름이 주어질때 도감번호 빠르게 찾기
-    }
-    for (int i = 0; i < m; i++){
-        cin >> temp;       
-        if(temp[0] >= 65 && temp[0] <= 90){ 
-        // 포켓몬 이름이 주어진 경우 (맨 앞 문자가 영어 대문자인것을 이용)
-            result.push_back(to_string(pokemon[temp]));
-            //result 벡터는 string이기 때문에 번호를 string으로 변환해줌
-        }
-        else // 도감 번호가 주어진 경우
-        {
-            result.push_back(name[stoi(temp)-1]);
-            //입력값은 string 그래서 정수형으로 변환 후 넣어줌
-            // -1을 해준 이유는 name배열은 0부터 시작했기 때문 
-        }
-    }
-    for (int i = 0; i < result.size();i++){
-        cout << result[i] << '\n'; // 출력
-    }
-    return 0;
+
+string str;
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+	int N, M;
+
+	map<string, int> mPoketmon;
+	map<int, string> mPoketmon2;
+
+	cin >> N >> M;
+
+	for (int i = 0; i < N; ++i)
+	{
+		cin >> str;
+		mPoketmon[str] = i + 1;
+		mPoketmon2[i + 1] = str;
+	}
+
+	for (int i = 0; i < M; ++i)
+	{
+		cin >> str;
+		if (atoi(str.c_str()) == 0)
+			cout << mPoketmon[str] << "\n";
+		else
+			cout << mPoketmon2[atoi(str.c_str())] << "\n";
+	}
+	return 0;
 }
