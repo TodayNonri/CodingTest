ba0c7c99521a9bb3c95e60f8bb75cca807a086cd
TodayNonri <126966916+TodayNonri@users.noreply.github.com>
2025-02-27 10:50:29 +0900

    [Platinum V] Title: 숨바꼭질 5, Time: 16 ms, Memory: 6060 KB -BaekjoonHub

--- 백준/Platinum/17071. 숨바꼭질 5/숨바꼭질 5.cc
+++ 백준/Platinum/17071. 숨바꼭질 5/숨바꼭질 5.cc
@@ -1,48 +1,55 @@
 #include <iostream>
 #include <queue>
 #include <vector>
-#include <cstring>  // memset 사용
+#include <cstring> 
 
 using namespace std;
 
 int N, K;
-int visited[500001][2];  // 방문 여부 체크 (짝수/홀수 구분)
+int visited[500001][2];  
 
-int bfs() {
-    queue<pair<int, int>> q; // (현재 위치, 현재 시간)
+int bfs()
+{
+    queue<pair<int, int>> q; 
     q.push({ N, 0 });
     visited[N][0] = 1;
 
-    while (!q.empty()) {
-        int now = q.front().first;
-        int time = q.front().second;
+    while (!q.empty()) 
+    {
+        int iNow = q.front().first;
+        int iTime = q.front().second;
         q.pop();
 
-        int brother = K + time * (time + 1) / 2;  // 동생 위치 계산
-        if (brother > 500000) return -1;  // 범위 초과 시 -1 반환
+        int brother = K + iTime * (iTime + 1) / 2;
+        if (brother > 500000) 
+            return -1; 
 
-        // 동생이 현재 시간에 방문한 곳을 수빈이가 같은 초에서 방문한 적 있으면 종료
-        if (visited[brother][time % 2]) return time;
+        if (visited[brother][iTime % 2]) 
+            return iTime;
 
-        for (int next : {now + 1, now - 1, now * 2}) {
-            if (next >= 0 && next <= 500000 && !visited[next][(time + 1) % 2]) {
-                visited[next][(time + 1) % 2] = 1;
-                q.push({ next, time + 1 });
+        for (int next : {iNow + 1, iNow - 1, iNow * 2})
+        {
+            if (next >= 0 && next <= 500000 && !visited[next][(iTime + 1) % 2]) 
+            {
+                visited[next][(iTime + 1) % 2] = 1;
+                q.push({ next, iTime + 1 });
             }
         }
     }
 
-    return -1;  // 동생을 못 찾은 경우
+    return -1;
 }
 
-int main() {
+int main() 
+{
     ios_base::sync_with_stdio(false);
     cin.tie(NULL); cout.tie(NULL);
 
     cin >> N >> K;
-    memset(visited, 0, sizeof(visited)); // 방문 배열 초기화
+    memset(visited, 0, sizeof(visited)); 
 
-    if (N == K) {
+    if (N == K) 
+    {
         cout << "0\n";
         return 0;
     }
