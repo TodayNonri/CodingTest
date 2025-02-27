3d7def21aa37f242f5ff45bd63837194336ac68e
TodayNonri <126966916+TodayNonri@users.noreply.github.com>
2025-02-28 08:51:05 +0900

    [Silver I] Title: 완전 이진 트리, Time: 0 ms, Memory: 2340 KB -BaekjoonHub

---
+++ 백준/Silver/9934. 완전 이진 트리/완전 이진 트리.cc
@@ -0,0 +1,55 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
+#include <math.h>
+
+using namespace std;
+
+int K;
+vector<int> vTree[10];
+vector<int> vNum;
+
+void CutNum(int iCenter, int iDepth, vector<int>& v)
+{
+    if (v.size() == 0)
+        return;
+
+    vTree[iDepth].push_back(v[iCenter]);
+
+    vector<int> vLeft;
+    vector<int> vRight;
+    for (int i = 0; i < iCenter; ++i)
+        vLeft.push_back(v[i]);
+    CutNum(vLeft.size() / 2, iDepth + 1, vLeft);
+
+    for (size_t i = iCenter+1; i < v.size(); ++i)
+        vRight.push_back(v[i]);
+    CutNum(vRight.size() / 2, iDepth + 1, vRight);
+
+    return;
+}
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(NULL);
+
+    cin >> K;
+    int N = pow(2, K)-1;
+    vNum.resize(N);
+    for (int i = 0; i < N; ++i)
+        cin >> vNum[i];
+    
+    int iCenter = (N / 2);
+
+    CutNum(iCenter, 0, vNum);
+
+    for (int i = 0; i < K; ++i)
+    {
+        for (auto j : vTree[i])
+            cout << j <<" ";
+        cout<<"\n";
+    }
+
+    return 0;
+}
