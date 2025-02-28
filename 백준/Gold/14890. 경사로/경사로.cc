585cdb7c9e09eae0f8ba0aefdb562036101e9c25
TodayNonri <126966916+TodayNonri@users.noreply.github.com>
2025-02-28 23:48:12 +0900

    [Gold III] Title: 경사로, Time: 0 ms, Memory: 2100 KB -BaekjoonHub

--- 백준/Gold/14890. 경사로/경사로.cc
+++ 백준/Gold/14890. 경사로/경사로.cc
@@ -1,29 +1,55 @@
-#include<bits/stdc++.h>
-using namespace std; 
-int n, l, a[104][104], b[104][104], ret; 
-void solve(int a[104][104]){
-    for(int i = 0; i < n; i++){
-        int cnt = 1; 
-        int j;  
-        for(j = 0; j < n - 1; j++){
-            if(a[i][j] == a[i][j + 1])cnt++; 
-            else if(a[i][j] + 1 == a[i][j + 1] && cnt >= l) cnt = 1; 
-            else if(a[i][j] - 1 == a[i][j + 1] && cnt >= 0) cnt = -l + 1; 
-            else break;
-        }
-        if(j == n - 1 && cnt >= 0) ret++; 
-    }
-    return; 
-} 
-int main(){
-    scanf("%d %d", &n, &l);
-    for(int i = 0; i < n; i++){ 
-        for(int j = 0; j < n; j++){
-            scanf("%d", &a[i][j]); 
-            b[j][i] = a[i][j];
-        }
-    }    
-    solve(a); solve(b);  
-    printf("%d\n", ret);  
-    return 0; 
+#include <iostream>
+#include <vector>
+#include <climits>
+
+using namespace std;
+
+int N, L, iResult = 0;
+int bridge1[101][101];
+int bridge2[101][101];
+
+void Check(int arr[101][101])
+{
+	for (int i = 0; i < N; ++i)
+	{
+		int iCount = 1;
+		int j;
+		for (j = 0; j < N - 1; ++j)
+		{
+			if (arr[i][j] == arr[i][j + 1])
+				iCount++;
+			else if (arr[i][j] + 1 == arr[i][j + 1] && iCount >= L)
+				iCount = 1;
+			else if (arr[i][j] - 1 == arr[i][j + 1] && iCount >= 0)
+				iCount = -L + 1;
+			else 
+				break;
+		}
+		if (j == N - 1 && iCount >= 0)
+			iResult++;
+	}
+	return;
+}
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL); cout.tie(NULL);
+	cin >> N >> L;
+
+	for (int i = 0; i < N; ++i)
+	{
+		for (int j = 0; j < N; ++j)
+		{
+			cin >> bridge1[i][j];
+			bridge2[j][i] = bridge1[i][j];
+		}
+	}
+
+	Check(bridge1);
+	Check(bridge2);
+
+	cout << iResult;
+	
+	return 0;
 }
