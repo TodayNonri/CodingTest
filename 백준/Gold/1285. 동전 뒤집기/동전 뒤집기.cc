a90528ae7653c6ba48605c6a8d9d2bb6854ba043
TodayNonri <126966916+TodayNonri@users.noreply.github.com>
2025-02-28 19:29:12 +0900

    [Gold I] Title: 동전 뒤집기, Time: 288 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Gold/1285. 동전 뒤집기/동전 뒤집기.cc
+++ 백준/Gold/1285. 동전 뒤집기/동전 뒤집기.cc
@@ -12,16 +12,13 @@ void CheckSwap(int iHere)
 	if (iHere == N + 1)
 	{
 		int iSum = 0;
-		for (int i = 1; i <= (1 << (N - 1)); i *= 2)
+		for (int i = 1; i < (1 << (N)); i *= 2)
 		{
 			int iCount = 0;
 			for (int j = 1; j <= N; ++j)
-			{
 				if (Coin[j] & i)
-				{
 					iCount++;
-				}
-			}
+
 			iSum += min(iCount, N - iCount);
 		}
 		iResult = min(iResult, iSum);
@@ -38,24 +35,20 @@ int main()
 	ios_base::sync_with_stdio(false);
 	cin.tie(NULL); cout.tie(NULL);
 	cin >> N;
-
-	for (int i = 1; i <= N; i++)
+	string strInput;
+	for (int i = 1; i <= N; ++i)
 	{
-		string strInput;
-		cin >> strInput;
+		cin>>strInput;
 		int iValue = 1;
-		for (int j = 0; j < N; j++)
+		for (int j = 0; j < N; ++j)
 		{
 			if (strInput[j] == 'T')
 				Coin[i] |= iValue;
-
 			iValue *= 2;
 		}
-
 	}
 
 	CheckSwap(1);
-
 	cout << iResult;
 
 	return 0;
