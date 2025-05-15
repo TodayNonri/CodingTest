ff11aeb40fab12d8e7da19cbe5e5862c63133f14
TodayNonri <126966916+TodayNonri@users.noreply.github.com>
2025-05-15 21:03:17 +0900

    [Gold III] Title: 사다리 조작, Time: 976 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Gold/15684. 사다리 조작/사다리 조작.cc
+++ 백준/Gold/15684. 사다리 조작/사다리 조작.cc
@@ -1,37 +1,35 @@
 #include <iostream>
+#include <algorithm>
 #include <vector>
+#include <climits>
 
 using namespace std;
+int N, M, H, iResult = INT_MAX;
+bool visited[34][34];
 
-int N, M, H, iResult = 100;
-
-bool visited[34][14] = { false, };
-
-bool Cal()
+bool check()
 {
     for (int i = 1; i <= N; ++i)
     {
         int iStart = i;
         for (int j = 1; j <= H; ++j)
         {
-            if (visited[j][iStart] == true)
+            if (visited[j][iStart])
                 iStart++;
-            else if (visited[j][iStart-1] == true)
+            else if (visited[j][iStart - 1])
                 iStart--;
         }
         if (iStart != i)
             return false;
     }
-
     return true;
 }
 
-void Sadari(int iHere, int iCount)
+void go(int iHere, int iCount)
 {
     if (iCount > 3 || iCount >= iResult)
         return;
-
-    if (Cal())
+    if (check())
     {
         iResult = min(iResult, iCount);
         return;
@@ -39,43 +37,36 @@ void Sadari(int iHere, int iCount)
 
     for (int i = iHere; i <= H; ++i)
     {
-        for (int j = 1; j <= N; ++j)
+        for (int j = 1; j < N; j++) 
         {
             if (visited[i][j] || visited[i][j - 1] || visited[i][j + 1])
                 continue;
-
-            visited[i][j] = true;
-            Sadari(i, iCount + 1);
-            visited[i][j] = false;
+            visited[i][j] = 1;
+            go(i, iCount + 1);
+            visited[i][j] = 0;
         }
     }
-
-    return;
 }
 
 int main()
 {
     ios_base::sync_with_stdio(false);
-    cin.tie(NULL); cout.tie(NULL);
-
+    cin.tie(NULL);    cout.tie(NULL);
     cin >> N >> M >> H;
 
-    int iInputY, iInputX;
+    int a, b;
     for (int i = 0; i < M; ++i)
     {
-        cin >> iInputY >> iInputX;
-        visited[iInputY][iInputX] = true;
+        cin >> a >> b;
+        visited[a][b] = 1;
     }
-
-    Sadari(1, 0);
-
-    if (iResult == 100)
+    go(1, 0);
+    if (iResult == INT_MAX)
     {
         cout << -1;
         return 0;
     }
-
     cout << iResult;
-
+    
     return 0;
 }
